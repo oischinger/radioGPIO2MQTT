f238b57432f7d5d9901af828e749800d4e83ecc3
Hans Oischinger <hans.oischinger@gmail.com>
2019-08-05 22:22:39 +0200

    Fix merge issues

--- run.sh
+++ run.sh
@@ -6,14 +6,7 @@ PASSWORD=$(jq --raw-output ".mqtt_password" $CONFIG_PATH)
 HOST=$(jq --raw-output ".mqtt_host" $CONFIG_PATH)
 PORT=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
 
-<<<<<<< HEAD
 echo Starting radioGPIO2MQTT
 python3 radioGPIO2MQTT.py $HOST $PORT $USERNAME $PASSWORD
 echo radioGPIO2MQTT has stopped
-=======
-echo Starting radio $HOST $PORT $USERNAME 
-python3 radioGPIO2MQTT.py $HOST $PORT $USERNAME $PASSWORD
-echo Radio started
->>>>>>> Switch to full access mode
-
 
