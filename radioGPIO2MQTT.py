782e2def5661395ce0444aff955444ceb225a163
Hans Oischinger <hans.oischinger@gmail.com>
2020-08-02 21:06:53 +0200

    Change birth message topic to Home assistant default

--- radioGPIO2MQTT.py
+++ radioGPIO2MQTT.py
@@ -55,11 +55,11 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("$SYS/#")
-    client.subscribe("hass/status")
+    client.subscribe("homeassistant/status")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.topic == "hass/status":
+    if msg.topic == "homeassistant/status":
         sendDiscover()
 
 HOST = sys.argv[1]
